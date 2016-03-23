ab6c7180e60d9978e46eca02b450630363d1db34
Raju Ahmmed <rajuahmmedbd@gmail.com>
2016-03-23 22:00:49 +0600

    Improved

--- single.php
+++ single.php
@@ -15,7 +15,7 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'single' ); ?>
+				<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
 
 				<?php the_post_navigation(); ?>
 
